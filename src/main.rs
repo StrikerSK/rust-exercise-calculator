0a77903d901e80f19d2406f0a22eb6532aa18e56
Teh|Striker <kristianhanus@gmail.com>
2023-01-02 20:51:49 +0100

    Looping when wrong input

--- src/main.rs
+++ src/main.rs
@@ -4,35 +4,39 @@ fn main() {
     println!("Hello user! I can calculate for you 1-RM, N-RM and Weight!");
     println!("Which one would you like to calculate (Max, Reps, Weight)?");
 
-    let binding = get_input_string().to_lowercase();
-    let calc_type = binding.as_str();
-    match calc_type {
+    loop {
+        let binding = get_input_string().to_lowercase();
+        let calc_type = binding.as_str();
+        match calc_type {
+            "max" => {
+                println!("Please enter used reps count");
+                let reps = get_input_integer();
+                println!("Please enter used weight");
+                let weight = get_input_float();
+                println!("Your one rep max is: {}", calculator::calculate_1rm(reps, weight));
+                break;
+            },
 
-        "max" => {
-            println!("Please enter used reps count");
-            let reps = get_input_integer();
-            println!("Please enter used weight");
-            let weight = get_input_float();
-            println!("Your one rep max is: {}", calculator::calculate_1rm(reps, weight));
-        },
+            "reps" => {
+                println!("Please enter your PR");
+                let pr = get_input_float();
+                println!("Please enter expected weight");
+                let weight = get_input_float();
+                println!("For weight {} kgs, you should execute: {} reps", weight, calculator::calculate_nrm(pr, weight));
+                break;
+            },
 
-        "reps" => {
-            println!("Please enter your PR");
-            let pr = get_input_float();
-            println!("Please enter expected weight");
-            let weight = get_input_float();
-            println!("For weight {} kgs, you should execute: {} reps", weight, calculator::calculate_nrm(pr, weight));
-        },
+            "weight" => {
+                println!("Please enter your PR");
+                let pr = get_input_float();
+                println!("Please enter expected reps count");
+                let reps = get_input_integer();
+                println!("Your training weight is: {}", calculator::calculate_used_weight(pr, reps));
+                break;
+            },
 
-        "weight" => {
-            println!("Please enter your PR");
-            let pr = get_input_float();
-            println!("Please enter expected reps count");
-            let reps = get_input_integer();
-            println!("Your training weight is: {}", calculator::calculate_used_weight(pr, reps));
-        },
-
-        _ => println!("Sorry, I don't know this calculation type!"),
+            _ => println!("Sorry, I don't know this calculation type! Do you want to calculate: Max, Reps or Weight"),
+        }
     }
 }
 
@@ -43,15 +47,27 @@ fn get_input_string() -> String {
 }
 
 fn get_input_integer() -> i32 {
-    match get_input_string().parse::<i32>() {
-        Ok(i) => i,
-        Err(..) => panic!("This element was not of Integer type"),
+    let mut input: String = get_input_string();
+    loop {
+        match input.parse::<i32>() {
+            Ok(i) => { return i },
+            Err(..) => {
+                println!("This element was not of Integer type! Please try again!");
+                input = get_input_string();
+            },
+        }
     }
 }
 
 fn get_input_float() -> f32 {
-    match get_input_string().parse::<f32>() {
-        Ok(i) => i,
-        Err(..) => panic!("This element was not of Float type"),
+    let mut input: String = get_input_string();
+    loop {
+        match input.parse::<f32>() {
+            Ok(i) => { return i },
+            Err(..) => {
+                println!("This element was not of Float type! Please try again!");
+                input = get_input_string();
+            },
+        }
     }
 }
