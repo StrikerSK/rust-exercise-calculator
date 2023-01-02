2134d2cff114f5927728be4921c1491070c4a316
Teh|Striker <kristianhanus@gmail.com>
2023-01-02 19:02:19 +0100

    Getting input

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,42 @@ use crate::calculator::calculate_used_weight;
 mod calculator;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Hello user! I can calculate for you 1-RM, N-RM and Training weight!");
+    println!("Which one would you like to calculate?");
 
-    let pr = calculator::calculate_1rm(5, 100.0);
+    println!("How many reps done?");
+    let mut reps: i32 = get_input_integer();
+    println!("Scanned reps value: {}", reps);
 
-    println!("1 rep max value: {}", pr);
-    println!("N rep max value: {}", calculator::calculate_nrm(pr, 100.0));
-    println!("Recommended weight value: {}", calculate_used_weight(pr, 5));
+    println!("How many weight used?");
+    let mut weight: f32 = get_input_float();
+    println!("Scanned weight value: {}", weight);
+
+    println!("What is your 1RM?");
+    let mut pr: f32 = get_input_float();
+    println!("Scanned pr value: {}", pr);
+
+    println!("Expected 1 rep max weight: {}", calculator::calculate_1rm(reps, weight));
+    println!("Expected reps for {} kgs value: {}", weight, calculator::calculate_nrm(pr, weight));
+    println!("Expected {} rep weight: {}", reps, calculator::calculate_used_weight(pr, reps));
+}
+
+fn get_input_string() -> String {
+    let mut input_text: String = String::new();
+    std::io::stdin().read_line(&mut input_text).unwrap();
+    return input_text.trim().to_string();
+}
+
+fn get_input_integer() -> i32 {
+    match get_input_string().parse::<i32>() {
+        Ok(i) => i,
+        Err(..) => panic!("This element was not of Integer type"),
+    }
+}
+
+fn get_input_float() -> f32 {
+    match get_input_string().parse::<f32>() {
+        Ok(i) => i,
+        Err(..) => panic!("This element was not of Float type"),
+    }
 }
