09fad717a1de04e9b2e0076a43ac28e928c50cf9
Teh|Striker <kristianhanus@gmail.com>
2023-01-03 10:41:29 +0100

    Calculation runner

--- src/calculations/mod.rs
+++ src/calculations/mod.rs
@@ -1,2 +1,3 @@
 pub mod wendler_calculator;
 pub mod exercise_calculator;
+pub mod calculator_runner;
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use calculations::exercise_calculator;
+use calculations::{calculator_runner, exercise_calculator};
 
 mod inputs;
 mod calculations;
@@ -8,35 +8,25 @@ fn main() {
     println!("Which one would you like to calculate (Max, Reps, Weight)?");
 
     loop {
-        // '&' solves issue with instantiating int trait object
-        let calculator: &dyn exercise_calculator::ExerciseCalculator = &calculations::wendler_calculator::WendlerCalculator {};
         let binding = inputs::get_input_string().to_lowercase();
         let calc_type = binding.as_str();
+
+        // '&' solves 'Sized' issue with instantiating int trait object
+        let calculator: &dyn exercise_calculator::ExerciseCalculator = &calculations::wendler_calculator::WendlerCalculator {};
+
         match calc_type {
             "max" => {
-                println!("Please enter used reps count");
-                let reps = inputs::get_input_integer();
-                println!("Please enter used weight");
-                let weight = inputs::get_input_float();
-                println!("Your one rep max is: {}", calculator.calculate_1rm(reps, weight));
+                calculator_runner::calculate_exercise_1rm(calculator);
                 break;
             },
 
             "reps" => {
-                println!("Please enter your PR");
-                let pr = inputs::get_input_float();
-                println!("Please enter expected weight");
-                let weight = inputs::get_input_float();
-                println!("For weight {} kgs, you should execute: {} reps", weight, calculator.calculate_nrm(pr, weight));
+                calculator_runner::calculate_exercise_nrm(calculator);
                 break;
             },
 
             "weight" => {
-                println!("Please enter your PR");
-                let pr = inputs::get_input_float();
-                println!("Please enter expected reps count");
-                let reps = inputs::get_input_integer();
-                println!("Your training weight is: {}", calculator.calculate_used_weight(pr, reps));
+                calculator_runner::calculate_exercise_weight(calculator);
                 break;
             },
 
